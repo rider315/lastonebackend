e857012d464b50a00117a0dccfe3e2d3f6e7d25c
Gaurav Chaudhary <jaatsaab735@gmail.com>
2024-12-16 19:49:40 +0530

    oi

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const paperRoutes = require('./routes/paperRoutes');
+// require('dotenv').config();
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/api', paperRoutes);
+
+// app.listen(5000, () => {
+//   console.log('Server is running on port 5000');
+// });
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -11,8 +29,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', paperRoutes);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+// Use the PORT environment variable or default to 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-
